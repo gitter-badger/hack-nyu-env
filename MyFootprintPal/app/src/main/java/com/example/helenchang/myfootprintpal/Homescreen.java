8c15e07e18388bbf43ecd91655d4a5f30a869444
Helen <hc1334@nyu.edu>
2016-02-20 08:47:32 -0500

    checkbox1 sort of works

--- MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/Homescreen.java
+++ MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/Homescreen.java
@@ -1,5 +1,6 @@
 package com.example.helenchang.myfootprintpal;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.view.View.OnClickListener;
+
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+//import com.jjoe64.graphview.
+
+
 
 public class Homescreen extends AppCompatActivity {
 
+    final int COL_TODAY = Color.GREEN;
+    final int COL_BEST = Color.GRAY;
+    final int COL_GOAL = Color.RED;
+
+    final DataPoint[] EMPTY = new DataPoint[]{new DataPoint(0,0)};
+
+    LineGraphSeries<DataPoint> todayData;
+    GraphView graph;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +46,79 @@ public class Homescreen extends AppCompatActivity {
             }
         });
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        //GRAPHS
+        graph = (GraphView) findViewById(R.id.graph);
+
+        //Line 1: Today's data
+        todayData = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
-        graph.addSeries(series);
+        graph.addSeries(todayData);
+        graph.setTitle("Today's Progress");
+        graph.setTitleTextSize(70);
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Carbon Footprint (CO2e)");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Time (h)");
+        todayData.setColor(COL_TODAY);
+
+
+
+        //Line 2: Best data
+        LineGraphSeries<DataPoint> bestData = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 2),
+                new DataPoint(1, 5),
+                new DataPoint(2, 4),
+                new DataPoint(3, 4),
+                new DataPoint(4, 7)
+        });
+        graph.addSeries(bestData);
+        bestData.setColor(COL_BEST);
+
+
+        //Line 3: Goal data
+        LineGraphSeries<DataPoint> goalData = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 5),
+                new DataPoint(1, 5),
+                new DataPoint(2, 5),
+                new DataPoint(3, 5),
+                new DataPoint(4, 5)
+        });
+        graph.addSeries(goalData);
+        goalData.setColor(COL_GOAL);
+
+
+        //CHECKBOXES
+        CheckBox todayCB = (CheckBox) findViewById(R.id.todayCB);
+        CheckBox bestCB = (CheckBox) findViewById(R.id.bestCB);
+        CheckBox goalCB = (CheckBox) findViewById(R.id.goalCB);
+
+        todayCB.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(((CheckBox)v).isChecked()){
+                    todayData.resetData(EMPTY);
+                    graph.addSeries(todayData);
+                }
+                else{
+                    todayData.resetData(new DataPoint[] {
+                            new DataPoint(0, 1),
+                            new DataPoint(1, 5),
+                            new DataPoint(2, 3),
+                            new DataPoint(3, 2),
+                            new DataPoint(4, 6)
+                    });
+
+                }
+            }
+
+
+
+        });
+
     }
 
     @Override
@@ -59,4 +142,7 @@ public class Homescreen extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
