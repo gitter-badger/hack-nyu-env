b9a87e186d6862439e742c41eb58f7a73c2b82a6
cabamiles <mc4486@nyu.edu>
2016-02-20 07:56:48 -0500

    Adding Update screen with functioning buttons

--- app/src/main/java/com/example/aaronwalker/myfootprintpal/Main.java
+++ app/src/main/java/com/example/aaronwalker/myfootprintpal/Main.java
@@ -1,5 +1,6 @@
 package com.example.aaronwalker.myfootprintpal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,16 +9,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
-
+    private static Button button_travel, button_water, button_food, button_waste;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_travel = (Button)findViewById(R.id.button);
+        button_water = (Button)findViewById(R.id.button2);
+        button_food = (Button)findViewById(R.id.button3);
+        button_waste = (Button)findViewById(R.id.button4);
+        OnClickTravelButtonListener();
+        OnClickWaterButtonListener();
+        OnClickFoodButtonListener();
+        OnClickWasteButtonListener();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +35,52 @@ public class Main extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void OnClickTravelButtonListener() {
+        button_travel.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Travel");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+    public void OnClickWaterButtonListener() {
+        button_water.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Water");
+                        startActivity(intent);
+                    }
+                }
+        );
+
+    }
+    public void OnClickFoodButtonListener() {
+        button_food.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Food");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+    public void OnClickWasteButtonListener() {
+        button_waste.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Waste");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
     @Override
