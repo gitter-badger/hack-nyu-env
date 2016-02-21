c9d5120fa716bf10b7731759ecc3909df632c22d
cabamiles <mc4486@nyu.edu>
2016-02-21 00:37:56 -0500

    Added expandable list view for update screen with buttons

--- app/src/main/java/com/example/aaronwalker/myfootprintpal/Main.java
+++ app/src/main/java/com/example/aaronwalker/myfootprintpal/Main.java
@@ -10,78 +10,66 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main extends AppCompatActivity {
-    private static Button button_travel, button_water, button_food, button_waste;
+    ExpandableListView expandableListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_travel = (Button)findViewById(R.id.button);
-        button_water = (Button)findViewById(R.id.button2);
-        button_food = (Button)findViewById(R.id.button3);
-        button_waste = (Button)findViewById(R.id.button4);
-        OnClickTravelButtonListener();
-        OnClickWaterButtonListener();
-        OnClickFoodButtonListener();
-        OnClickWasteButtonListener();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        expandableListView = (ExpandableListView) findViewById(R.id.exp_listview);
+        final List<String> Headings = new ArrayList<String>();
+        List<String> L1 = new ArrayList<String>();
+        List<String> L2 = new ArrayList<String>();
+        List<String> L3 = new ArrayList<String>();
+        final HashMap<String, List<String>> ChildList = new HashMap<String, List<String>>();
+        String heading_items[] = getResources().getStringArray(R.array.updates);
+        String l1[] = getResources().getStringArray(R.array.h1_items);
+        String l2[] = getResources().getStringArray(R.array.h2_items);
+        String l3[] = getResources().getStringArray(R.array.h3_items);
+        for (String title : heading_items) {
+            Headings.add(title);
+        }
+        for (String title : l1) {
+            L1.add(title);
+        }
+        for (String title : l2) {
+            L2.add(title);
+        }
+        for (String title : l3) {
+            L3.add(title);
+        }
+        ChildList.put(Headings.get(0), L1);
+        ChildList.put(Headings.get(1), L2);
+        ChildList.put(Headings.get(2), L3);
+        final MyAdapter myAdapter = new MyAdapter(this, Headings, ChildList);
+        expandableListView.setAdapter(myAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getBaseContext(),
+                        "update " + Headings.get(groupPosition) + " for today!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                Toast.makeText(getBaseContext(), ChildList.get(Headings.get(childPosition)) + "", Toast.LENGTH_LONG).show();
+
+                return false;
             }
         });
     }
 
-    public void OnClickTravelButtonListener() {
-        button_travel.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Travel");
-                        startActivity(intent);
-                    }
-                }
-        );
-    }
-    public void OnClickWaterButtonListener() {
-        button_water.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Water");
-                        startActivity(intent);
-                    }
-                }
-        );
-
-    }
-    public void OnClickFoodButtonListener() {
-        button_food.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Food");
-                        startActivity(intent);
-                    }
-                }
-        );
-    }
-    public void OnClickWasteButtonListener() {
-        button_waste.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("com.example.aaronwalker.myfootprintpal.Waste");
-                        startActivity(intent);
-                    }
-                }
-        );
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
