b6bf3dae6695ef55bcd5a48b493fe3edf8f8cb34
Helen <hc1334@nyu.edu>
2016-02-21 04:46:10 -0500

    added questions

--- MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/DisplayQuestionnaire.java
+++ MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/DisplayQuestionnaire.java
@@ -138,7 +138,7 @@ public class DisplayQuestionnaire extends FragmentActivity {
 
         // Set the text in the new row to a random country.
         ((TextView) newView.findViewById(android.R.id.text1)).setText(
-                QUESTIONS[(str) (Math.random() * COUNTRIES.length)]);
+                questions.get(0));
 
         // Set a click listener for the "X" button in the row that will remove the row.
         newView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
@@ -161,6 +161,23 @@ public class DisplayQuestionnaire extends FragmentActivity {
         mContainerView.addView(newView, 0);
     }
 
-    
+    private void populateQuestions(){
+        questions.add("How many people live in your house");
+        questions.add("What kind of energy system does your house use?");
+        questions.add("Do you use energy efficient light bulbs?");
+        questions.add("What kind of stove system does your house use");
+        questions.add("How often do you cook?");
+        questions.add("How many times do you shower a day");
+        questions.add("What is your primary form of transportation");
+        questions.add("If you drive a car, what kind of car do you drive?");
+        questions.add("If you take public transportation, which one do you take?");
+        questions.add("How far do you commute?");
+        questions.add("How many times a year do you fly out of the US");
+        questions.add("How many times a year do you fly within the US?");
+        questions.add("What is your diet like?");
+        questions.add("How often do you drink bottled water (yearly)?");
+
+    }
+
 
 }
