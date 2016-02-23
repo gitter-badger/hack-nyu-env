d874dbfec4723fd0fb99176196732b3522c5b503
awwalker <awwalker3@gmail.com>
2016-02-22 23:20:26 -0500

    SWIPE WORK DONT BREAK

--- MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/CustomSwipeAdapter.java
+++ MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/CustomSwipeAdapter.java
@@ -12,7 +12,20 @@ import android.widget.TextView;
  * Created by aaronwalker on 2/22/16.
  */
 public class CustomSwipeAdapter extends PagerAdapter {
-    private int[] question_resources = {R.string.question1};
+    private String[] question_resources = {"How many people live in your house?",
+            "What kind of energy system does your house use?",
+            "Do you use energy efficient light bulbs?",
+            "What kind of stove system does your house use",
+            "How often do you cook?",
+            "How many times do you shower a day?",
+            "What is your primary form of transportation?",
+            "If you drivea a car: What kind of car do you drive?",
+            "If you take public transportation do you take the train or metro?",
+            "If you take a car or public transportation how far do you commute?",
+            "How many times a year do you fly out of the United States?",
+            "How many times a year did you fly within the United States?",
+            "What is your diet like?",
+            "How often do you drink bottled water? (yearly)"};
     private Context ctx;
     private LayoutInflater layoutInflater;
     public CustomSwipeAdapter(Context ctx){
@@ -35,9 +48,9 @@ public class CustomSwipeAdapter extends PagerAdapter {
         View item_view = layoutInflater.inflate(R.layout.swipe_layout, container, false);
         TextView textView = (TextView) item_view.findViewById(R.id.text1);
 
-        textView.setText("Question: " +position);
+        textView.setText(question_resources[position]);
         container.addView(item_view);
-        return super.instantiateItem(container, position);
+        return item_view;
     }
 
     public void destroyItem(ViewGroup container, int position, Object object){
--- MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/DisplayQuestionnaire.java
+++ MyFootprintPal/app/src/main/java/com/example/helenchang/myfootprintpal/DisplayQuestionnaire.java
@@ -1,36 +1,18 @@
 package com.example.helenchang.myfootprintpal;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-
+import android.app.Activity;
 import android.os.Bundle;
-
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.content.Intent;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
-public class DisplayQuestionnaire extends FragmentActivity {
+public class DisplayQuestionnaire extends Activity {
     ViewPager viewPager;
     CustomSwipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homescreen);
+        setContentView(R.layout.swipe_layout);
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         adapter = new CustomSwipeAdapter(this);
         viewPager.setAdapter(adapter);
